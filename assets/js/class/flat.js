9e7eb8d0996318c447f3498348e261b88597f6eb
Carlos Delgado <crlsdlgd1992@gmail.com>
2024-10-14 13:14:48 -0500

    add objects

---
+++ assets/js/class/flat.js
@@ -0,0 +1,12 @@
+class Flat {
+    constructor(city, streetName, streetNumber, areaSize, hasAC, yearBuild, rentPrice, dateAvailable) {
+        this.city = city;
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+        this.areaSize = areaSize;
+        this.hasAC = hasAC;
+        this.yearBuild = yearBuild;
+        this.rentPrice = rentPrice;
+        this.dateAvailable = dateAvailable;
+    }
+}
---
+++ assets/js/class/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(email, password, firstName, lastName, birthDate) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.favoriteFlats = [];
+    }
+}
