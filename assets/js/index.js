f0aa15063717fea27bf2a51b30a20aace6d393fb
Carlos Delgado <crlsdlgd1992@gmail.com>
2024-10-14 12:25:24 -0500

    plantillas listas

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,13 @@ const toggleMenu = () => {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('hidden');
 }
+
+const toggleUserMenu = () => {
+    const userMenu = document.querySelector('#user-menu');
+    userMenu.classList.toggle('hidden');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const [email] = document.getElementsByName('email');
+    email.value = 'qwe@qwe.com';
+})
